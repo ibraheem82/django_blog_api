da1aa655eeeb664ed5f7d541a4666b33ed50b1c3
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-05-10 17:15:02 +0100

    This commit adds two new fields, first_name and last_name, to the serializer. These fields allow users to enter their first name and last name when registering for an account.

--- blog_api/accounts/serializer.py
+++ blog_api/accounts/serializer.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 class RegisterSerializer(serializers.Serializer):
+  first_name = serializers.CharField()
+  last_name = serializers.CharField()
   username = serializers.CharField()
   password = serializers.CharField()
 
