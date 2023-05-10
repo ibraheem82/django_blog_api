8de2fe16e5b2756909321b2adb3dc6b0a2fca059
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-05-10 10:35:02 +0100

    Commit message: Add RegisterView API endpoint with error handling
    Explanation: In this commit, I added a new API endpoint called RegisterView that accepts HTTP POST requests. The endpoint takes in user data and uses the RegisterSerializer to validate the data. If the data is valid, the endpoint will create a new user account. If the data is invalid, the endpoint will return a JSON response with an error message and a 400 Bad Request status code.
    
    I also added error handling to the endpoint to handle any unexpected exceptions that may occur during the processing of the request. If an exception occurs, the endpoint will return a JSON response with an error message and a 400 Bad Request status code.

--- blog_api/accounts/views.py
+++ blog_api/accounts/views.py
@@ -3,3 +3,14 @@ from rest_framework.response import Response
 from .serializers import RegisterSerializer
 from rest_framework import status
 # Create your views here.
+def RegisterView(APIView):
+  
+  def post(self, request):
+    try:
+      data = request.data
+      serializer = RegisterSerializer(data = data)
+      if not serializer.is_valid():
+        return Response({
+          'data': serializer.errors()
+          'message': 'something went wrong'
+        }, status = status.HTTP_400_BAD_REQUEST)
