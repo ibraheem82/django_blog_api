39f49ec0c2c1fbf2fb204d164edd3bd8cc7daa72
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-05-12 19:30:57 +0100

    correctly add {Field}

--- blog_api/home/models.py
+++ blog_api/home/models.py
@@ -5,7 +5,7 @@ import uuid
 from django.contrib.auth.models import User
 
 class BaseModel(models.Model):
-  uid = models.UUIDFiled(primary_key=True, editable=False, default=uuid.uuid4)
+  uid = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
   created_at = models.DateField(auto_now_add=True)
   updated_at = models.DateField(auto_now_add=True)
 
