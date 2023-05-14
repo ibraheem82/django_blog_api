be65e052b7a40287edf0e47819c90cbda2ac3a95
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-05-14 11:49:22 +0100

    Imported IsAuthenticated class from rest_framework.permissions module. This permission class can be used to ensure that the user making the API request is authenticated.
    Imported JWTAuthentication class from rest_framework_simplejwt.authentication module. This authentication class can be used to authenticate API requests using JSON Web Tokens (JWT).

--- blog_api/home/views.py
+++ blog_api/home/views.py
@@ -2,5 +2,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import BlogSerializer
 from rest_framework import status
-
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 # Create your views here.
