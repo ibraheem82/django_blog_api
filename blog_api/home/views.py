600fd655a26f356b90aa519183557474b4ab9f03
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-05-14 11:01:51 +0100

    Implemented APIView subclass from the rest_framework.views module to handle API requests. Imported the Response class from rest_framework.response module to send responses from the API endpoints. Added import statement for BlogSerializer from the .serializer module to handle serialization and deserialization of Blog model data. Included import statement for the status module from rest_framework to access HTTP status codes for API responses.

--- blog_api/home/views.py
+++ blog_api/home/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializer import BlogSerializer
+from rest_framework import status
 
 # Create your views here.
