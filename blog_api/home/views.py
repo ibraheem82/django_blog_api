62c8c5277f7d7b0e3645e73af679beb305f7b72b
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-05-14 11:50:09 +0100

    Defined a BlogView class that extends the APIView class from the rest_framework.views module.
    Set the permission_classes attribute to [IsAuthenticated], which means that only authenticated users will have permission to access this view.
    
    Set the authentication_classes attribute to [JWTAuthentication], which specifies that JSON Web Token authentication will be used for this view.
    
    Implemented a post method within the BlogView class. This method handles HTTP POST requests sent to the view.
    
    Inside the post method, the request data is retrieved using request.data. Then, a BlogSerializer instance is created, passing the data to it.
    
    If an exception occurs during this process, the exception is caught and printed for debugging purposes.

--- blog_api/home/views.py
+++ blog_api/home/views.py
@@ -5,3 +5,13 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 # Create your views here.
+
+class BlogView(APIView):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+    def post(self, request):
+      try:
+        data = request.data
+        serializer = BlogSerializer(data = data)
+      except Exception as e:
+        print(e)  
