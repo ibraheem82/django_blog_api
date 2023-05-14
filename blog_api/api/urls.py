a48748cee5ac8aa8d256d79c0416674764f75faf
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-05-14 11:47:59 +0100

    , removed

--- blog_api/api/urls.py
+++ blog_api/api/urls.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from django.urls import path, include
 urlpatterns = [
     path('account/', include('accounts.urls')),
-    path('home/', include('home.urls')),
+    path('home/', include('home.urls'))
 ]
