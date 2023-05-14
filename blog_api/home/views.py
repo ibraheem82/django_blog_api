eb5da102bb65cd47d23bb94269dd4f118b2f2b76
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-05-14 11:48:37 +0100

    Imported BlogSerializer from the current module's .serializers module. This allows for using the BlogSerializer class to serialize and deserialize Blog model instances within the current module.

--- blog_api/home/views.py
+++ blog_api/home/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializer import BlogSerializer
+from .serializers import BlogSerializer
 from rest_framework import status
 
 # Create your views here.
